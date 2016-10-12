4efdccae10601cb00044b5bc5c9ace9763f428b9
chenbin <chenbin@chinamobile.com>
2016-10-12 13:58:41 +0800

    robolectric 单元测试

--- app/src/test/java/com/cb/qiangqiang2/ExampleUnitTest.java
+++ app/src/test/java/com/cb/qiangqiang2/ExampleUnitTest.java
@@ -1,15 +1,28 @@
 package com.cb.qiangqiang2;
 
-import org.junit.Test;
+import android.widget.EditText;
 
-import static org.junit.Assert.*;
+import com.cb.qiangqiang2.ui.activity.MainActivity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+        EditText editText = new EditText(RuntimeEnvironment.application);
+        editText.setText("haha");
+        Assert.assertEquals("haha", editText.getText().toString());
     }
 }
